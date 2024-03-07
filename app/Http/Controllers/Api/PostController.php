1a6762f1b999389c814a1eacd94069c678f47b87
tomasdarienko <darienkotomas@gmail.com>
2024-03-07 21:19:28 +0100

    milestone 1

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::paginate(6);
+
+
+        return response()->json([
+
+            'success' => true,
+            'results' =>  $posts,
+
+        ]);
+}
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controllers\Api\PostController as PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/posts',[PostController::class , 'index']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
